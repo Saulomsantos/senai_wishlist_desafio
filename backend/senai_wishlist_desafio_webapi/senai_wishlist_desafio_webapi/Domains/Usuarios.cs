53f3028a8b17c43829b2ea02c06db8521b14acd0
Saulo Santos <saulo.macedoprojetos@gmail.com>
2019-04-09 11:31:19 -0300

    atualizando backend depois dos conflitos de merge

---
+++ backend/senai_wishlist_desafio_webapi/senai_wishlist_desafio_webapi/Domains/Usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace senai_wishlist_desafio_webapi.Domains
+{
+    public partial class Usuarios
+    {
+        public Usuarios()
+        {
+            Desejos = new HashSet<Desejos>();
+        }
+
+        public int Id { get; set; }
+        public int TipoUsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        public TiposUsuarios TipoUsuario { get; set; }
+        public ICollection<Desejos> Desejos { get; set; }
+    }
+}
